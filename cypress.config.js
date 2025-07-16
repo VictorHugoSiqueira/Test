d549b447c2726241c8cc315dd89462349c8dd0a8
victor.siqueira <Victor.Siqueira@partner.bmwgroup.com>
2025-07-15 22:37:36 -0300

    add variables to .env

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,14 @@
 const { defineConfig } = require('cypress');
+require('dotenv').config();
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://front.serverest.dev',
+    env: {
+      nome: process.env.CYPRESS_USER,
+      senha: process.env.CYPRESS_PASSWORD,
+      senhaInvalida: process.env.CYPRESS_INVALID_PASSWORD
+    },
     setupNodeEvents(on, config) {
     }
   }
