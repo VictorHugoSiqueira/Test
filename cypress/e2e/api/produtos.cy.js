4a0e10038f333cb2735c00918cdf7b69585bf606
victor.siqueira <Victor.Siqueira@partner.bmwgroup.com>
2025-07-15 23:07:21 -0300

    fix assertions on api tests

--- cypress/e2e/api/produtos.cy.js
+++ cypress/e2e/api/produtos.cy.js
@@ -20,10 +20,10 @@ describe('Testes da API de Produtos', () => {
     });
 
     it('Deve listar produtos com sucesso', () => {
-        cy.request(`${baseUrl}/produtos`).then((response) => {
+        cy.request(`${baseUrl}/produtos?_id=BeeJh5lz3k6kSIzA`).then((response) => {
             expect(response.status).to.eq(200);
-            expect(response.body.quantidade).to.eq(3);
-            expect(response.body.produtos[0].nome).to.eq('microsoft windows');
+            expect(response.body.quantidade).to.eq(1);
+            expect(response.body.produtos[0].nome).to.eq('Logitech MX Vertical');
             expect(response.body.produtos[0].preco).to.eq(470);
         });
     });
@@ -36,9 +36,9 @@ describe('Testes da API de Produtos', () => {
     });
 
     it('Listar carrinhos cadastrados', () => {
-        cy.request(`${baseUrl}/carrinhos`).then((response) => {
+        cy.request(`${baseUrl}/carrinhos?_id=ZtpWT89eDJpp0K6z`).then((response) => {
             expect(response.status).to.eq(200);
-            expect(response.body.quantidade).to.eq(1);
+            expect(response.body.quantidade).to.eq(0);
         });
     });
 });
